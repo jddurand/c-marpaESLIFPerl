4905195c9dfc316f33a6e3e0b5d9208b8a1c96e2
Jean-Damien Durand <jeandamiendurand@free.fr>
2024-02-19 08:28:29 +0100

    etc/init_pause.pl: Fix

--- etc/init_pause.pl
+++ etc/init_pause.pl
@@ -10,7 +10,12 @@ my ($user, $heyhey) = @ARGV;
 my $pause = File::Spec->catfile(File::HomeDir->my_home, '.pause');
 print "Initializing $pause\n";
 open(my $fd, '>', $pause) || die "Cannot open $pause, $!";
-print $fd "$user\n$heyhey\n";
+if ($user) {
+    print $fd "user $user\n";
+}
+if ($password) {
+    print $fd "password $password\n";
+}
 close($fd) || warn "Cannot close $pause, $!";
 
 exit(EXIT_SUCCESS);
